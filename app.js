adf94d769f628d2b5ba3ecdaf4576c7195c26fcb
Chris <chrissouth89@gmail.com>
2020-02-02 21:54:25 -0600

    first draft dice roller

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
+$(() => {
+    $.ajax({
+        url: "http://www.dnd5eapi.co/api/classes",
+        type: "GET",
+        
+
+    }).done(function(data) {
+        console.log("Retrieved " + data + " records from dataset!")
+        console.log(data);
+    });
+});
 // on blur event
 
 // local storage set value
-var charName = $('#charname').val();
-var playerName = $('#playername').val();
-var charClass = $('#class').val();
-var race = $('#race').val();
-var background = $('#back').val();
-var alignment = $('#align').val();
-var exp = $('#exp').val();
-var level = $('#level').val();
-localStorage.setItem("charname", charName);
-localStorage.setItem("playername", playerName);
-localStorage.setItem("class", charClass);
-localStorage.setItem("race", race);
-localStorage.setItem("back", background);
-localStorage.setItem("align", alignment);
-localStorage.setItem("exp", exp);
-localStorage.setItem("level", level);
+// const addClass = (addClass) => {
+//     addClass.preventDefault();
+//     let addClass = {
+//         id: "class",
+        
+//     }
+
+// }
+
+function rollDice() {
+    const dice = [...document.querySelectorAll(".die-list")];
+    dice.forEach(die => {
+      toggleClasses(die);
+      die.dataset.roll = getRandomNumber(1, 6);
+    });
+  }
+  
+  function toggleClasses(die) {
+    die.classList.toggle("odd-roll");
+    die.classList.toggle("even-roll");
+  }
+  
+  function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  document.getElementById("roll-button").addEventListener("click", rollDice);
 
 
 // show dice animated or show dice spinning without numbers landing on number shown or choosing math.random showing spinning dice.
