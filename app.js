6fb6e558e906e861af8c9fad0fc2d18fd773c0a4
Chris <chrissouth89@gmail.com>
2020-02-04 20:54:10 -0600

    changes

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
 $(() => {
-	var face=1;
+    $.ajax({
+		url: "https://api.open5e.com/classes",
+        type: "GET",
+	}).done(function(data) {
+		console.log(data)
+    })
+
+var face = 1;
 
 var show = function() {
-  $('.cube1').attr('class', 'cube cube1 show'+face);
-  if(face==6) {
-    face=1;
+  $('.cube1').attr('class', 'cube cube1 show'+ face);
+  if(face == 6) {
+    face = 1;
   } else {
-    face++; 
+    face ++; 
   }
 };
 
 var show2 = function() {
-  $('.cube2').attr('class', 'cube cube2 show'+face);
-  if(face==6) {
-    face=1;
+  $('.cube2').attr('class', 'cube cube2 show'+ face);
+  if(face == 6) {
+    face = 1;
   } else {
-    face++; 
+    face ++; 
   }
 };
 
 $('.cube1').on('click', show);
 $('.cube2').on('click', show2);
 
-$.ajax({
-		url: "http://www.dnd5eapi.co/api/classes",
-		type: "GET",
-	}).done(function(data) {
-		console.log("Retrieved " + data + " records from dataset!")
-		console.log(data)
-    })
-    
 })
 
 
