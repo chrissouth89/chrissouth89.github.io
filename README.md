6f12d56a132c2592e217b358bc86053501f9970f
Chris <chrissouth89@gmail.com>
2020-02-05 17:46:18 -0600

    readme update

--- README.md
+++ README.md
@@ -3,11 +3,18 @@
 # Concept: 
 Interactive D&amp;D Character sheet with dice roller and auto filling information.
 
-# Layout:
-Standard 5th edition character sheet
+# Objective:
+Use an API to call various information to aid in character creation of a dungeons and dragons character.
 
 # Elements: 
-Input character information
+  ** Input field for class.
 
-* Buttons:
-  * Dice rolling
+* Input:
+  ** Enter class name.
+
+* Buttons: 
+  ** Submit class input to retrieve class information.
+
+* Click:
+  ** On click dice will roll and show a random value.
+
--- app.js
+++ app.js
@@ -8,10 +8,12 @@ $(() => {
             url:'https://api.open5e.com/classes/' + userInput
         }).then(
             (data) => {
-            $('#overview').text(data.desc)
-            $('#startingskills').text(data.prof_skills)
-            $('#startinghp').text(data.hp_at_1st_level)
-            $('#equipment').text(data.equipment)
+            $('#overview').html(data.desc)
+            $('#hitdice').html(data.hit_dice)
+            $('#startinghp').html(data.hp_at_1st_level)
+            $('#savingthrows').html(data.prof_saving_throws)
+            $('#startingskills').html(data.prof_skills)
+            $('#equipment').html(data.equipment)
         },
         (data) => {
             console.log("Something went wrong")
@@ -42,7 +44,3 @@ var show2 = function() {
 $('.cube1').on('click', show);
 $('.cube2').on('click', show2);
 });
-
-
-
-// show dice animated or show dice spinning without numbers landing on number shown or choosing math.random showing spinning dice.
