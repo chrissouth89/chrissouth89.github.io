2f8f2926c083ed15420d59dd002490ed38b4d2c3
Chris <chrissouth89@gmail.com>
2020-02-05 18:24:51 -0600

    fully up to date

--- app.js
+++ app.js
@@ -1,46 +1,47 @@
 $(() => {
-    $("form").on("submit", (event)=>{
-        event.preventDefault();
+	$("form").on("submit", event => {
+		event.preventDefault()
 
-        const userInput = $('input[type="text"]').val();
+		const userInput = $('input[type="text"]').val()
 
-        $.ajax({
-            url:'https://api.open5e.com/classes/' + userInput
-        }).then(
-            (data) => {
-            $('#overview').html(data.desc)
-            $('#hitdice').html(data.hit_dice)
-            $('#startinghp').html(data.hp_at_1st_level)
-            $('#savingthrows').html(data.prof_saving_throws)
-            $('#startingskills').html(data.prof_skills)
-            $('#equipment').html(data.equipment)
-        },
-        (data) => {
-            console.log("Something went wrong")
-        }
-        )
-    })
+		$.ajax({
+			url: "https://api.open5e.com/classes/" + userInput,
+		}).then(
+			data => {
+				console.log(data)
+				$("#overview h1 strong span").html(data.desc)
+				$("#hitdice h1 strong span").html(data.hit_dice)
+				$("#startinghp h1 strong span").html(data.hp_at_1st_level)
+				$("#savingthrows h1 strong span").html(data.prof_saving_throws)
+				$("#startingskills h1 strong span").html(data.prof_skills)
+				$("#equipment h1 strong span").html(data.equipment)
+			},
+			data => {
+				console.log("Something went wrong")
+			}
+		)
+	})
 
-var face = 1;
+	var face = 1
 
-var show = function() {
-  $('.cube1').attr('class', 'cube cube1 show'+ face);
-  if(face == 6) {
-    face = 1;
-  } else {
-    face ++; 
-  }
-};
+	var show = function() {
+		$(".cube1").attr("class", "cube cube1 show" + face)
+		if (face == 6) {
+			face = 1
+		} else {
+			face++
+		}
+	}
 
-var show2 = function() {
-  $('.cube2').attr('class', 'cube cube2 show'+ face);
-  if(face == 6) {
-    face = 1;
-  } else {
-    face ++; 
-  }
-};
+	var show2 = function() {
+		$(".cube2").attr("class", "cube cube2 show" + face)
+		if (face == 6) {
+			face = 1
+		} else {
+			face++
+		}
+	}
 
-$('.cube1').on('click', show);
-$('.cube2').on('click', show2);
-});
+	$(".cube1").on("click", show)
+	$(".cube2").on("click", show2)
+})
